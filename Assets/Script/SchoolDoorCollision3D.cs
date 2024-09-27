c9a44b7b28ea621db6525a1d5f1fc1326eddd2b4
fnilvuwu <mamatmks45@gmail.com>
2024-09-27 22:56:21 +0800

    init

---
+++ Assets/Script/SchoolDoorCollision3D.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SchoolDoorCollision3D : MonoBehaviour
+{
+    [SerializeField]
+    private string nextSceneName = "MainScene"; // Name of the scene to load, you can set this in the Inspector too
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Detected");  // Log when something enters the trigger
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the trigger");  // Log when the player enters the trigger
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.Log("Something else entered the trigger: " + other.gameObject.name);
+        }
+    }
+}
